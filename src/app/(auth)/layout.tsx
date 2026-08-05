533feb8315fbf6134a2c8c466def2e2340f26cc6
nonsothetrader <nonsothetrader@gmail.com>
2026-08-05 21:42:40 +0100

    Update layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,16 +1,18 @@
-
 import Link from 'next/link';
 import { Logo } from '@/components/logo';
+import { FirebaseProvider } from '@/firebase';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-secondary">
-      <div className="mb-8">
-        <Link href="/">
-          <Logo className="h-40 w-40 text-primary" />
-        </Link>
-      </div>
-      {children}
+      <FirebaseProvider>
+        <div className="mb-8">
+          <Link href="/">
+            <Logo className="h-40 w-40 text-primary" />
+          </Link>
+        </div>
+        {children}
+      </FirebaseProvider>
     </div>
   );
 }
