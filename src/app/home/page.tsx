db5e2d632133976c9028bd2a8972d533c5b0ac26
nonsothetrader <nonsothetrader@gmail.com>
2026-08-05 22:34:22 +0100

    Add files via upload

---
+++ src/app/home/page.tsx
@@ -0,0 +1,31 @@
+
+'use client';
+
+import * as React from 'react';
+import { Header } from '@/components/landing/header';
+import { Hero } from '@/components/landing/hero';
+import { HowItWorks } from '@/components/landing/how-it-works';
+import { WeeklyMenuPreview } from '@/components/landing/weekly-menu-preview';
+import { Testimonials } from '@/components/landing/testimonials';
+import { Pricing } from '@/components/landing/pricing';
+import { FreeTrial } from '@/components/landing/free-trial';
+import { Faq } from '@/components/landing/faq';
+import { Footer } from '@/components/landing/footer';
+
+export default function HomePage() {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="flex-1">
+        <Hero />
+        <HowItWorks />
+        <WeeklyMenuPreview />
+        <Pricing />
+        <FreeTrial />
+        <Testimonials />
+        <Faq />
+      </main>
+      <Footer />
+    </div>
+  );
+}
