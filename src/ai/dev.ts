784d208f73a5a64854aebbd7b53e823a522a5c14
nonsothetrader <nonsothetrader@gmail.com>
2026-08-05 20:41:51 +0100

    Add files via upload

---
+++ src/ai/dev.ts
@@ -0,0 +1,6 @@
+import { config } from 'dotenv';
+config();
+
+import '@/ai/flows/surprise-meal-recommendation.ts';
+import '@/ai/flows/witty-confirmation-flow.ts';
+import '@/ai/flows/process-order-flow.ts';
