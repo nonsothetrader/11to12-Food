3b5e200efffa7640617f795c4cab04dac21ab75b
nonsothetrader <nonsothetrader@gmail.com>
2026-08-05 20:59:14 +0100

    Add files via upload

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9079771298-a052f",
+  "appId": "1:312421754497:web:ae10b4ea5962cc7c620f80",
+  "apiKey": "AIzaSyAWdNbDQPhmQF4PnM3RS8mHrfN4oOpE_2M",
+  "authDomain": "studio-9079771298-a052f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "312421754497"
+};
